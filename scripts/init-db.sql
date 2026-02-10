460c9117661a4a6b9a801a13766525f564920937
quentinchirol <157401619+quentinchirol@users.noreply.github.com>
2026-02-10 09:50:05 +0100

    Refactor SQL script for table

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -1,14 +1,14 @@
--- Création de la table Players si elle n'existe pas déjà
+-- Création de la table Players
 CREATE TABLE IF NOT EXISTS Players (
     player_id INT PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
-    email VARCHAR(200),
+    email VARCHAR(200) NULL, 
     registration_date DATE NOT NULL,
     country VARCHAR(100),
     level INT DEFAULT 1
 );
 
--- Création de la table Scores si elle n'existe pas déjà
+-- Création de la table Scores
 CREATE TABLE IF NOT EXISTS Scores (
     score_id VARCHAR(20) PRIMARY KEY,
     player_id INT NOT NULL,
@@ -18,8 +18,7 @@ CREATE TABLE IF NOT EXISTS Scores (
     played_at DATETIME NOT NULL,
     platform VARCHAR(50),
     CONSTRAINT fk_player
-        FOREIGN KEY (player_id)
+        FOREIGN KEY (player_id) 
         REFERENCES Players(player_id)
         ON DELETE CASCADE
-
 );
